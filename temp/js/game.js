20007088211cfd78f54457c99fd1990183270ddb
Aff1ic7i0n <Aff1ic7i0n@live.com>
2014-07-17 15:36:20 -0700

    Res fixed to 720p

--- temp/js/game.js
+++ temp/js/game.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("game"),
 	context = canvas.getContext("2d"),
 	frameRequestId = null;
 
-canvas.width = 500;
+canvas.width = 1280;
 canvas.height = 720;
 
 var lastTime = 0;
@@ -115,10 +115,10 @@ function draw(dt) {
 	player.draw(context);
 
 	
-	console.clear();
-	console.log("fVertical:       " + fVertical.toFixed(2) + " (N)");
-	console.log("fVerticalDrag:   " + fVerticalDrag.toFixed(2) + " (N)");
-	console.log("fHorizontal:     " + (fHorizontal - fHorizontalDrag).toFixed(2)+ " (N)");
-	console.log("player velocity: " + player.velocityX.toFixed(2) + " | " + player.velocityY.toFixed(2) + " (m/s)");
-	console.log("player position: " + (player.x / PIXELS_PER_METER).toFixed(2) + " | " + (player.y / PIXELS_PER_METER).toFixed(2) + " (m)");
+//	console.clear();
+//	console.log("fVertical:       " + fVertical.toFixed(2) + " (N)");
+//	console.log("fVerticalDrag:   " + fVerticalDrag.toFixed(2) + " (N)");
+//	console.log("fHorizontal:     " + (fHorizontal - fHorizontalDrag).toFixed(2)+ " (N)");
+//	console.log("player velocity: " + player.velocityX.toFixed(2) + " | " + player.velocityY.toFixed(2) + " (m/s)");
+//	console.log("player position: " + (player.x / PIXELS_PER_METER).toFixed(2) + " | " + (player.y / PIXELS_PER_METER).toFixed(2) + " (m)");
 }
