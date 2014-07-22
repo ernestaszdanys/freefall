f42125ced96534e076cb6e39d289fc0a95ed163b
julius-bagdonas <totally.not.julius@gmail.com>
2014-07-22 16:52:56 -0700

    Point - rectangle intersection check

--- game/js/physics.js
+++ game/js/physics.js
@@ -1,6 +1,7 @@
-var Intersections = {
-	pointRect : function(){
-	
+var Intersection = {
+	pointRect : function(x, y, rect){
+		return (x >= rect.x && x <= rect.x + rect.width)
+				&& (y >= rect.y && y <= rect.y + rect.height);
 	},
 	circlePoly : function(){
 	
