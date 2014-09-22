1c52f0e62822104523e5e04ca895e74e60138cb3
julius-bagdonas <totally.not.julius@gmail.com>
2014-09-22 12:38:38 +0300

    Correct description grammar.

--- game/js/social_networks.js
+++ game/js/social_networks.js
@@ -15,7 +15,7 @@ function openFbPopUp(score) {
                 link: 'http://37.157.0.95/game/game/',
                 picture: window.location + 'assets/images/logo.png',
                 caption: 'Points collected: ' + score,
-                description: 'FREE FALL - features challenging physics-based gameplay and hours of replay value. Get ready for an epic falling journy in SPACE. Avoid rocks and magnets, try to get as more points as you can, but remember the further you go, the faster it gets! MORE COMING SOON! STAY TUNED!'
+                description: 'FREE FALL - features challenging physics-based gameplay and hours of replay value. Get ready for an epic falling journy in SPACE. Avoid rocks and magnets, try to get as much points as you can, but remember the further you go, the faster it gets! MORE COMING SOON! STAY TUNED!'
             },
             function(response) {
                 if (response && response.post_id) {
