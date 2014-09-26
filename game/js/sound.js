1d33ad39807ded38588eee3a81f224c0d4cb0c3f
ernestaszdanys <ernestas13@gmail.com>
2014-09-26 16:30:27 +0300

    Working Sound class

--- game/js/sound.js
+++ game/js/sound.js
@@ -1,22 +1,32 @@
-function Sound(audioContext, soundArrayBuffer, volume) {
-    var decodedAudio,
-        gainNode = audioContext.createGain(),
-        soundSource = audioContext.createBufferSource();
+function Sound(audioContext, soundArrayBuffer, vol) {
+    var volume = vol,
+        decodedAudio,
+        gainNode,
+        soundSource;
 
     audioContext.decodeAudioData(soundArrayBuffer, function(buffer) {
         decodedAudio = buffer;
     });
 
-    //soundSource.loop = true;
-    gainNode.gain.value = volume;
-    soundSource.connect(gainNode);
-    gainNode.connect(audioContext.destination);
-    soundSource.start();
-
     // play
     this.play = function() {
+        if (soundSource) {
+            soundSource.disconnect(0);
+        }
+        if (gainNode) {
+            gainNode.disconnect(0);
+        }
+
+        gainNode = audioContext.createGain();
+        gainNode.gain.value = volume;
+        soundSource = audioContext.createBufferSource();
         soundSource.buffer = decodedAudio;
+
+        soundSource.connect(gainNode);
+        gainNode.connect(audioContext.destination);
+        soundSource.start();
     };
+
     // stop
     this.stop = function () {
         soundSource.stop();
